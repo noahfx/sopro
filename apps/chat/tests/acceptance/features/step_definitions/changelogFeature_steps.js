3a160a57894f06a43423e5d1546d12bd57a1d653
jimmymorales <jimmy@centralservices.io>
2015-02-11 10:55:43 -0600

    Added Changelog.md

---
+++ apps/chat/tests/acceptance/features/step_definitions/changelogFeature_steps.js
@@ -0,0 +1,30 @@
+var sharedSteps = module.exports = function(){
+
+  this.Given(/^I have downloaded the Society Pro source code$/, function (next) {
+    next();
+  });
+
+  this.When(/^I list the source files$/, function (next) {
+    next();
+  })
+
+  this.Then(/^I should see a file named CHANGELOG.md$/, function (next) {
+  	var glob = require("glob");
+
+  	glob("CHANGELOG.md", {}, function (er, files) {
+		// files is an array of filenames.
+		// If the `nonull` option is set, and nothing
+		// was found, then files is ["**/*.js"]
+		// er is an error object or null.
+		if (er) {
+			next.fail(new Error(er));
+		} else {
+			if (files.length == 0) {
+				next.fail(new Error("CHANGELOG not found"));
+			} else {
+				next();	
+			}
+		}
+	});
+  });
+}
