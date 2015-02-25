819c252d6bf8a90538f85c39d41306f91e958dbe
jimmymorales <jimmy@centralservices.io>
2015-02-25 10:42:00 -0600

    Remove ZombieJS from world in acceptance tests

--- apps/chat/tests/acceptance/features/support/world.js
+++ apps/chat/tests/acceptance/features/support/world.js
@@ -1,14 +1,9 @@
 // features/support/world.js
 module.exports = function() {
   
-  var zombie = require('zombie');
   //zombie.localhost('localhost', 8080);
   this.World = function World(callback) {
-    this.browser = new zombie(); // this.browser will be available in step definitions
-
-    this.visit = function(url, callback) {
-      this.browser.visit(url, callback);
-    };
+    //this.browser = new zombie(); // this.browser will be available in step definitions
 
     callback(); // tell Cucumber we're finished and to use 'this' as the world instance
   };
