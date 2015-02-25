c210fdad42a6a627c6648f4162985bd6a595919d
jimmymorales <jimmy@centralservices.io>
2015-02-25 10:17:55 -0600

    Fix gui and acceptance jenkins script

--- apps/chat/tests/acceptance/.jenkins.sh
+++ apps/chat/tests/acceptance/.jenkins.sh
@@ -1,9 +1,11 @@
+~/vert.x-2.1.5/bin/vertx run -cluster mock-backend.vertx.js  &
+
 sudo /etc/init.d/xvfb start
 
 sudo /etc/init.d/selenium start
 
 # lift the vertx app
-~/vert.x-2.1.5/bin/vertx run server.js &
+~/vert.x-2.1.5/bin/vertx run -cluster server.js &
 
 sleep 15
 
@@ -13,4 +15,6 @@ sudo npm run cucumber
 cat ./tests/acceptance/report.json | ./node-modules/.bin/cucumber-junit > tests/acceptance/test-out.xml 
 
 # stop vertx
+pkill -n java
+
 pkill -n java
