409afbea668e035954835b96c1efbf07e5beaf42
jimmymorales <jimmy@centralservices.io>
2015-03-04 14:53:24 -0600

    Acceptance tests

---
+++ apps/chat/tests/acceptance/features/step_definitions/channelSubscriberDropdown_steps.js
@@ -0,0 +1,20 @@
+var CAM_MOCKS = require('../../../mock-data.js');
+var SA_STEPS = require('../../sharedAPI_steps.js');
+
+var channelSubscriberDropdown_steps = module.exports = function(){
+  this.When(/^I select a channel from the list$/, function (next) {
+    element.all(by.css("#collection-channels .channel-item")).get(0).click()
+    .then(next);
+  });
+
+  this.Then(/^I should see a list of channel subscribers$/, function (next) {
+    element(by.css("#subscribers-dropdown li")).isDisplayed()
+    .then(function(isDisplayed) {
+      if (isDisplayed) {
+        next();
+      } else {
+        next.fail(new Error("Subscribers Dropdown is not being displayed"));
+      }
+    });
+  });
+}
