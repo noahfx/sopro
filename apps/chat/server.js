36b3d31b056538f0ae482df7f5079145f12b9666
Jimmy Morales <jimmymorales26@gmail.com>
2015-03-13 10:39:42 -0600

    Deploy backend from server

--- apps/chat/server.js
+++ apps/chat/server.js
@@ -2,7 +2,8 @@
 CAM = {};
 
 var vertx = require('vertx');
-console = require('vertx/console');
+console = require('vertx/console'),
+container = require('vertx/container');
 
 var server = vertx.createHttpServer();
 var eb = vertx.eventBus;
@@ -13,6 +14,9 @@ CAM.eb = eb;
 
 load('routes.js');
 load('eb-messengers.js');
+
+
+container.deployVerticle("mock-backend.vertx.js");
 
 // ATM our server does not have any message handlers at all; all communication with the EB is req/rep from eb-messengers.js.
 //load('eb-handlers.js');
