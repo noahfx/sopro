5c9f4c9e4a208996fb22e587b94fcb4291fdacae
plato-cambrian <plato@cambrian.org>
2015-03-17 11:36:36 -0600

    Remove sudo from npm because it broke all the things

--- apps/chat/tests/acceptance/.jenkins.sh
+++ apps/chat/tests/acceptance/.jenkins.sh
@@ -1,7 +1,6 @@
 # Ensure nvm binary selection is respected by sudo:
 export NVM_DIR="/home/ubuntu/.nvm"
 . "$NVM_DIR/nvm.sh"
-alias sudo='sudo '
 
 nvm use 0.12
 
@@ -12,14 +11,14 @@ sudo /etc/init.d/selenium start
 # lift the vertx app
 ~/vert.x-2.1.5/bin/vertx run server.js &
 
-sleep 5
+sleep 10
 
-sudo npm start &
+npm start &
 
 sleep 5
 
 # run protractor GUI tests
-sudo npm run acceptance
+npm run acceptance
 
 # stop vertx
 pkill -n java
--- apps/chat/tests/api_client/.jenkins.sh
+++ apps/chat/tests/api_client/.jenkins.sh
@@ -1,19 +1,20 @@
 # Ensure nvm binary selection is respected by sudo:
 export NVM_DIR="/home/ubuntu/.nvm"
 . "$NVM_DIR/nvm.sh"
-alias sudo='sudo '
 
 nvm use 0.12
+nvm alias default 0.12
+
 ~/vert.x-2.1.5/bin/vertx run server.vertx.js &
 
+sleep 10
+
+
+npm start &
+
 sleep 5
 
-
-sudo npm start &
-
-sleep 5
-
-sudo npm run api
+npm run api
 
 # stop vertx
 
