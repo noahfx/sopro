8c00bbdabc4d3c95e17401baccaf6b8290a56750
plato-cambrian <plato@cambrian.org>
2015-03-17 09:52:07 -0600

    Configure vertx server with config file. Correct jenkins.sh files.

--- apps/chat/tests/gui/.jenkins.sh
+++ apps/chat/tests/gui/.jenkins.sh
@@ -1,18 +1,19 @@
-~/vert.x-2.1.5/bin/vertx run -cluster mock-backend.vertx.js  &
 
 sudo /etc/init.d/xvfb start
 
 sudo /etc/init.d/selenium start
 
 # lift the vertx app
-~/vert.x-2.1.5/bin/vertx -cluster run server.js &
+~/vert.x-2.1.5/bin/vertx run server.vertx.js &
 
-sleep 15
+sleep 5
+
+npm start
 
 # run protractor GUI tests
 sudo npm run gui
 
 # stop vertx
-pkill -n java
 
 pkill -n java
+pkill -n node
