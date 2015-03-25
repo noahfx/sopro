afede814ab79c0fea972a3ba19933223ad73fe49
plato-cambrian <plato@cambrian.org>
2015-03-25 17:30:06 -0600

    Looks like it was in the other protractor.conf.js too

--- apps/chat/tests/acceptance/protractor.conf.js
+++ apps/chat/tests/acceptance/protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
 
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
   
   resultJsonOutputFile: 'tests/acceptance/report.json',
   
