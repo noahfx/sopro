95392631895bd06b427f04d67836d7e2128b0b1f
plato-cambrian <plato@cambrian.org>
2015-04-07 18:21:50 -0600

    More jenkins files improvements

---
+++ apps/chat/tests/unit/backend/.jenkins.sh
@@ -0,0 +1,5 @@
+npm install
+cd couchdb
+node populate-couchdb-mocks.js
+cd ..
+npm run unit-backend
--- apps/chat/tests/unit/frontend/.jenkins.sh
+++ apps/chat/tests/unit/frontend/.jenkins.sh
@@ -1,11 +1,3 @@
-#export NVM_DIR="/home/ubuntu/.nvm"
-#. "$NVM_DIR/nvm.sh"
-#
-#nvm use 0.10
-#nvm alias default 0.10
-
 npm install
 #RUNS KARMA
-npm run unit
-#RUNS MOCHA
-#npm run mocha
+npm run unit-frontend
