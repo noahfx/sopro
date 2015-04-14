811605ca4bc4a3649cb3eeec696e9fe5301b6f08
voodoo <voodoo@blackandyellowpages.com>
2015-04-14 13:29:02 -0600

    added missing acceptance test step

--- apps/chat/tests/acceptance/features/step_definitions/channelSubscriberDropdown_steps.js
+++ apps/chat/tests/acceptance/features/step_definitions/channelSubscriberDropdown_steps.js
@@ -1,8 +1,7 @@
 var CAM_MOCKS = require('../../../common/mock-data.js');
-var SA_STEPS = require('../../sharedAPI_steps.js');
+var SSTEPS = require('../../shared_steps.js');
 
-this.Given(SA_STEPS.appStarted.regex,
-    SA_STEPS.appStarted.fn)
+this.Given(SSTEPS.appStarted.regex, SSTEPS.appStarted.fn)
 
 var channelSubscriberDropdown_steps = module.exports = function(){
   this.When(/^I select a channel from the list$/, function (next) {
