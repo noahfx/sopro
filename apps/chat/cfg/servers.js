1798ba237c6f67fb45d33d0fb6ef039eab3ea85f
plato <plato@cambrian.org>
2015-04-22 14:53:22 -0600

    Update servers.js

--- apps/chat/cfg/servers.js
+++ apps/chat/cfg/servers.js
@@ -11,14 +11,14 @@ module.exports = CAM_CONFIG_SERVERS = {
       certfile: 'cfg/ssl/192.168.10.206.crt',
       fingerprint: '1E:DE:9A:80:2D:98:51:D9:25:A5:F3:A4:3B:BD:89:21:7C:9D:DB:A2',
     },
-    runtimeGroup: "noahfx",
-    runtimeUser: "noahfx"
+    runtimeGroup: "ubuntu",
+    runtimeUser: "ubuntu"
   },
   couchdb: {
     port: 5984,
     db: 'mocks',
-    host: '192.168.10.206',
-    url: 'http://192.168.10.206:5984',
+    host: 'localhost',
+    url: 'http://localhost:5984',
     //url: "https://admin:sopassword@sopro.iriscouch.com",
     //host: "sopro.iriscouch.com",
   },
