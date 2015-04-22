b9f81a96f9aa3a90009bbc5a024dc8e5d05aaaeb
voodoo <voodoo@blackandyellowpages.com>
2015-04-22 13:38:18 -0600

    some debug text

--- apps/chat/tests/acceptance/features/step_definitions/newUserActivationLink_steps.js
+++ apps/chat/tests/acceptance/features/step_definitions/newUserActivationLink_steps.js
@@ -60,6 +60,7 @@ module.exports = function(){
   this.When(/^I visit the confirm account page for that token$/,function (next) {
     var self = this;
     var uri = 'https://' + this.app.sopro.servers.express.hostname + '/confirmAccount/' + this.tokenObj.token
+    console.log(uri);
     browser.driver.get(uri)
     .then(function () {
       browser.driver.getPageSource()
