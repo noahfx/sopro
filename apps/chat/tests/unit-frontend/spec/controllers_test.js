a2f07a0ed70c93782992b14933f363d44d9762fd
plato-cambrian <plato@cambrian.org>
2015-04-22 14:47:24 -0600

    Improve whitespace

--- apps/chat/tests/unit-frontend/spec/controllers_test.js
+++ apps/chat/tests/unit-frontend/spec/controllers_test.js
@@ -9,19 +9,20 @@ describe("societyProChat Controllers", function() {
 
   beforeEach(module("societyProChatApp"));
 
-  beforeEach(inject(function ($rootScope, $controller, $httpBackend) {
-      httpBackend = $httpBackend;
-      scope = $rootScope.$new();
-      //scope.currentUser = require('./couchdb/mocks/user1')
-      rootscope = $rootScope;
-      UserServiceMock = {
-        identities: [
-          {
-            "identityid":"abc",
-          }
-        ]
-      }
-      createController = function(name) {
+  beforeEach(
+    inject(
+      function ($rootScope, $controller, $httpBackend) {
+        httpBackend = $httpBackend;
+        scope = $rootScope.$new();
+        rootscope = $rootScope;
+        UserServiceMock = {
+          identities: [
+            {
+              "identityid":"abc",
+            }
+          ]
+        }
+        createController = function(name) {
           name = name || 'mainController';
           var controller = $controller(name, {
             '$scope': scope,
@@ -31,10 +32,10 @@ describe("societyProChat Controllers", function() {
             httpBackend.flush();
           }
           return controller;
-      };
-  }));
-
-
+        };
+      }
+    )
+  );
 
   describe('main controller', function(){
     beforeEach(function(){
@@ -67,55 +68,56 @@ describe("societyProChat Controllers", function() {
 
 
   describe('card controller', function(){
-		var controller;
+    var controller;
     beforeEach(function(){
-			scope.card = {
-				channel : {_id : "unit_test_channel"}
-				};
-			controller = createController('historyCardController');
+      scope.card = {
+        channel : {
+          _id: "unit_test_channel"
+        }
+      };
+      controller = createController('historyCardController');
       httpBackend.expect('GET', '/api/channel.history?channel=unit_test_channel')
-					.respond(CAM_MOCKS.channelHistoryResponse);
-			httpBackend.flush();
+        .respond(CAM_MOCKS.channelHistoryResponse);
+      httpBackend.flush();
     })
     afterEach(function() {
-        httpBackend.verifyNoOutstandingExpectation();
-        httpBackend.verifyNoOutstandingRequest();
+      httpBackend.verifyNoOutstandingExpectation();
+      httpBackend.verifyNoOutstandingRequest();
     });
 
     it("has an array of messages", function () {
-
       expect(scope.messages).not.toBeUndefined();
       expect(Object.prototype.toString.call(scope.messages)).toEqual('[object Array]');
     });
-
 
     it("loads messages", function () {
-
       expect(scope.messages).not.toBeUndefined();
       expect(Object.prototype.toString.call(scope.messages)).toEqual('[object Array]');
-		  expect(scope.messages[0].text).toBe("New");
-		  expect(scope.messages[1].text).toBe("Old");
+      expect(scope.messages[0].text).toBe("New");
+      expect(scope.messages[1].text).toBe("Old");
     });
 
-			describe("Sort Function", function(){
-					var mockMessage;
-				it("exists", function () {
-				mockMessage = CAM_MOCKS.channelHistoryResponse.messages[0];
-					expect(scope.sortByTs).not.toBeUndefined();
-					expect(typeof scope.sortByTs).toEqual('function');
+    describe("Sort Function", function(){
+      var mockMessage;
+      it("exists", function () {
+        mockMessage = CAM_MOCKS.channelHistoryResponse.messages[0];
+        expect(scope.sortByTs).not.toBeUndefined();
+        expect(typeof scope.sortByTs).toEqual('function');
+      });
+
+      it("returns a Number timestamp from a message", function(){
+        var result = scope.sortByTs(mockMessage);
+        expect(result).toBe(Number(mockMessage.ts));
+      });
+
+      it("throws an exception for a timestamp that can not be cast to Number", function(){
+        function badSort(){
+          scope.sortByTs({ts : undefined});
+        };
+        expect(badSort).toThrow();
+      });
     });
-			it("returns a Number timestamp from a message", function(){
-				var result = scope.sortByTs(mockMessage);
-			  expect(result).toBe(Number(mockMessage.ts));
-			});
-		it("throws an exception for a timestamp that can not be cast to Number", function(){
-				function badSort(){
-		scope.sortByTs({ts : undefined});
-			};
-			expect(badSort).toThrow();
-				});
-			});
-			});
+  });
 
   describe('stage controller', function(){
     beforeEach(function (){
@@ -136,7 +138,6 @@ describe("societyProChat Controllers", function() {
       var controller = createController('stageController');
       //scope.stageCards.push({creationTitle: "karmachannel"});
 
-
       spyOn(scope, 'handleCreateChannelClicked');
       rootscope.$broadcast('createChannelClicked');
       setTimeout(function(){
@@ -152,16 +153,16 @@ describe("societyProChat Controllers", function() {
       httpBackend.expect('POST', '/api/channel?name=karmachannel')
         .respond(CAM_MOCKS.postChannelResponse);
       var controller = createController('stageController');
-      
+
       scope.$parent.channels = [];
       scope.$parent.myChannels = [];
-      
+
       scope.creationCard.creationTitle = "karmachannel";
 
       var channelsLength1 = rootscope.channels.length;
 
       // Click the Create button on the first card:
-      scope.createClicked(0); 
+      scope.createClicked(0);
       httpBackend.flush();
       var channelsLength2 = rootscope.channels.length;
 
@@ -191,4 +192,4 @@ describe("societyProChat Controllers", function() {
     })
 
   });
-	});
+});
